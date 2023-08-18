a61605b9eb1f599e8a5684b4b662bc66acaac5a1
ppondeu <106013516+ppondeu@users.noreply.github.com>
2023-08-19 02:46:43 +0700

    Update detection.py

--- detection.py
+++ detection.py
@@ -7,18 +7,24 @@ from PIL import Image
 protopath = "MobileNetSSD_deploy.prototxt"
 modelpath = "MobileNetSSD_deploy.caffemodel"
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
-# Only enable it if you are using OpenVino environment
-# detector.setPreferableBackend(cv2.dnn.DNN_BACKEND_INFERENCE_ENGINE)
-# detector.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
-
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
            "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
            "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
            "sofa", "train", "tvmonitor"]
-# cv2 resize window
-cv2.namedWindow("Application", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("Application", 1280, 600)
+
+
+def passing_percentage(opp_count, team_count):
+    if opp_count == 0 and team_count == 0:
+        return f"Free space"
+    elif opp_count == 0 and team_count > 0:
+        return f"Free to pass"
+    elif opp_count > team_count:
+        return f"Beware opponent is near"
+    elif opp_count < team_count:
+        return f"Depends on your decision"
+    else:
+        return f"Depends on your decision"
 
 def get_limits(color):
     c = np.uint8([[color]])  # BGR values
@@ -50,6 +56,8 @@ def main():
     fps = 0
     total_frames = 0
     cnt = 0
+    opp_count = 0
+    team_count = 0
     while True:
         ret, frame = cap.read()
         frame = imutils.resize(frame, width=600)
@@ -62,12 +70,14 @@ def main():
         detector.setInput(blob)
         person_detections = detector.forward()
         count = 0
+        opp_count = 0
+        team_count = 0
 
         for i in np.arange(0, person_detections.shape[2]):
           
             confidence = person_detections[0, 0, i, 2]
          
-            if confidence > 0.5:
+            if confidence > 0.8:
                 idx = int(person_detections[0, 0, i, 1])
 
                 if CLASSES[idx] != "person":
@@ -80,25 +90,26 @@ def main():
                         result = frame[startY:endY, startX:endX]
 
                         if result.any():
-                            cnt += 1
                             hsvImage = cv2.cvtColor(result, cv2.COLOR_BGR2HSV)
 
                             lowerLimit, upperLimit = get_limits(color=red)
-                            
-                            # print(lowerLimit,upperLimit)
 
                             mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
                             redd = cv2.bitwise_and(result,result,mask=mask)
-                            mask_ = Image.fromarray(mask)
+                            # mask_ = Image.fromarray(mask)
 
                             non_black_pixels = np.sum(np.any(redd != [0, 0, 0], axis=1))
                             if non_black_pixels > 100:
                                 cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 0, 255), 2)
-                                print(f"Detecting Red {non_black_pixels}")
+                                # print(f"Detecting Red {non_black_pixels}")
+                                team_count += 1
+                                cv2.putText(frame, f"Person {team_count}", (startX, startY - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
                             else:
                                 cv2.rectangle(frame, (startX, startY), (endX, endY), (255, 0, 0), 2)
-                                print(f"Not Detecting Red {non_black_pixels}")
-
+                                # print(f"Not Detecting Red {non_black_pixels}")
+                                opp_count += 1
+                                cv2.putText(frame, f"Person {opp_count}", (startX, startY - 5), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 0, 0), 1)
+                            
         fps_end_time = datetime.datetime.now()
         time_diff = fps_end_time - fps_start_time
         if time_diff.seconds == 0:
@@ -110,12 +121,16 @@ def main():
 
 
         cv2.putText(frame, fps_text, (5, 30), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 0, 255), 1)
-        count = 0
+        txt = passing_percentage(opp_count, team_count)
+        cv2.putText(frame, txt, (5, 60), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (0, 255, 255), 1)
 
         cv2.imshow("Application", frame)
         key = cv2.waitKey(1)
         if key == ord('q'):
             break
+        print(txt)
+        print(f"blue : {opp_count}")
+        print(f"red : {team_count}")
 
 
     cv2.destroyAllWindows()
